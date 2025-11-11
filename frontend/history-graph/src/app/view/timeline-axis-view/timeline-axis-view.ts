65d2687a35b0e667da14507652c34d20c15c3266
Michael Lindner <mlindner9@yahoo.com>
2025-11-11 17:11:46 -0500

    Icons for start and end markers.

--- frontend/history-graph/src/app/view/timeline-axis-view/timeline-axis-view.ts
+++ frontend/history-graph/src/app/view/timeline-axis-view/timeline-axis-view.ts
@@ -1,26 +1,50 @@
-import { Component, inject, input, Signal } from '@angular/core';
-import { Point2D, Rect2D } from '../../graphics/gfx-coord-2d';
+import { Component, computed, inject, input, signal, Signal } from '@angular/core';
+import { INVALID_POSITION_SENTINEL, Point2D, Rect2D, Size2D } from '../../graphics/gfx-coord-2d';
 import { DEFAULT_LINE_STYLE, DEFAULT_TEXT_STYLE, LineStyle, TextStyle } from '../../graphics/gfx-style';
 import { AxisLayoutService } from '../../services/axis-layout.service';
+import { SvgIcon, SvgIconOrigin } from '../svg-icon/svg-icon';
 
 @Component({
-  selector: '[tl-axis]',
-  templateUrl: './timeline-axis-view.html',
-  styleUrl: './timeline-axis-view.css'
+  	selector: '[tl-axis]',
+	imports: [SvgIcon],
+	templateUrl: './timeline-axis-view.html',
+  	styleUrl: './timeline-axis-view.css'
 })
 export class TimelineAxisView {
 	private axisLayoutService = inject(AxisLayoutService);
 
+	// Expose types for template
+	SvgIconOrigin = SvgIconOrigin;
+	
 	// Content
 	startLabel = input.required<string>();
 	endLabel = input.required<string>();
 	
 	// Positioning
+	markerSize = signal(new Size2D(16));
 	get startPos(): Signal<Point2D> {
 		return this.axisLayoutService.startPos;
 	}
 	get endPos(): Signal<Point2D> {
 		return this.axisLayoutService.endPos;
+	}
+	get startIconPos(): Signal<Point2D> {
+		return computed(() => {
+			const pos = this.startPos();
+			if (this.displayBounds().contains(pos)) {
+				return pos;
+			}
+			return new Point2D(INVALID_POSITION_SENTINEL, INVALID_POSITION_SENTINEL);
+		});
+	}
+	get endIconPos(): Signal<Point2D> {
+		return computed(() => {
+			const pos = this.endPos();
+			if (this.displayBounds().contains(pos)) {
+				return pos;
+			}
+			return new Point2D(INVALID_POSITION_SENTINEL, INVALID_POSITION_SENTINEL);
+		});
 	}
 	get displayBounds(): Signal<Rect2D> {
 		return this.axisLayoutService.displayBounds;
