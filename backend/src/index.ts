0a4d12032c2286c6a32db83c1c39a15d45e12140
Michael Lindner <mlindner9@yahoo.com>
2025-12-19 13:50:36 -0500

    Backend - 1) Prompt improvements. 2) Refactor response formatting into separate function.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,19 +12,20 @@ const app = express();
 // Disable CORS.
 app.use(cors());
 
-app.get('/api/generate-events', async (req, res) => {
+app.get('/api/generate-timeline', async (req, res) => {
 	try {
 		const prompt = decodeURIComponent(req.query.prompt as string);
-		const formatting = 'For each event, provide the start date, end date (if applicable), a label and a short description. Format the response as a JSON array.';
-		console.log(prompt);
+		console.debug("Prompt: ", prompt);
+
+		const formatting = 'Format the response as a JSON object. The JSON object should have title for the query, a start and and end date for the covered time period, and an array of events. The events should be in the format of a JSON array of objects. For each event, provide the start date, end date (if applicable), a label and a short description. The dates should be in the format YYYY-MM-DD if the day is known, in the format YYYY-MM if the month is known, or YYYY if the year is known. Years with less than 4 digits should *not* be padded with zeros. For historic date use a negative year number for BC. Sort the events in chronological order. Do not include any other text in the response.';
+
 		const response = await ai.models.generateContent({
 			model: "gemma-3-27b-it",
 			contents: `${prompt} ${formatting}`,
 		});
-		console.log(response.text);
-		const text = response.text || '[]';
-		const cleanText = text.replace(/```json/g, '').replace(/```/g, '').trim();
-		res.json(JSON.parse(cleanText));
+		console.debug("Response: ", response.text);
+
+		res.json(formatResponse(response.text));
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
@@ -34,3 +35,10 @@ app.get('/api/generate-events', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+function formatResponse(response: string | undefined): any {
+	if (!response) {
+		throw new Error("Response is undefined");
+	}
+	return JSON.parse(response.replace(/```json/g, '').replace(/```/g, '').trim());
+}
