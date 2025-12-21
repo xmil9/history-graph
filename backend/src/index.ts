484d867bb84e268c2f4507536bf43e4bfb884fb0
Michael Lindner <mlindner9@yahoo.com>
2025-12-20 19:35:52 -0500

    Backend - More detailed instructions for data format.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,14 +14,14 @@ app.use(cors());
 
 app.get('/api/generate-timeline', async (req, res) => {
 	try {
-		const prompt = decodeURIComponent(req.query.prompt as string);
-		console.debug("Prompt: ", prompt);
+		const topic = decodeURIComponent(req.query.topic as string);
+		console.debug("Topic: ", topic);
 
-		const formatting = 'Format the response as a JSON object. The JSON object should have title for the query, a start and and end date for the covered time period, and an array of events. The events should be in the format of a JSON array of objects. For each event, provide the start date, end date (if applicable), a label and a short description. The dates should be in the format YYYY-MM-DD if the day is known, in the format YYYY-MM if the month is known, or YYYY if the year is known. Years with less than 4 digits should *not* be padded with zeros. For historic date use a negative year number for BC. Sort the events in chronological order. Do not include any other text in the response.';
+		const instructions = `Generate a timeline for the topic: "${topic}". Format the response as a JSON object. The timeline should have a title for the topic, a start and and end date for the covered time period, and an array of events. The key for the title should be "title". The key for the timeline start date should be "start_date". The key for the timeline end date should be "end_date". The key for the events should be "events". The events should be in the format of a JSON array of objects. For each event, provide the start date, end date (if there is one), a label and a short description. The key for the event start date should be "start_date". The key for the event end date should be "end_date". The key for the event label should be "label". The key for the event description should be "description". The dates should be in the format YYYY-MM-DD if the day is known, in the format YYYY-MM if the month is known, or YYYY if the year is known. Years with less than 4 digits should *not* be padded with zeros. For historic dates use a negative year number for BC. Sort the events in chronological order. Do not include any other text in the response.`;
 
 		const response = await ai.models.generateContent({
 			model: "gemma-3-27b-it",
-			contents: `${prompt} ${formatting}`,
+			contents: `${instructions}`,
 		});
 		console.debug("Response: ", response.text);
 
